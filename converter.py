a07ee37fc1c917b7f06502feedb872b08ab57608
Roberto DeDeus <roberto.p.dedeus@gmail.com>
2020-03-06 15:44:45 -0500

    Initial commit.

---
+++ converter.py
@@ -0,0 +1,54 @@
+def meters_to_feet(meters: float) -> float:
+    """
+
+    :param meters:
+    :return:
+    """
+    pass  # implement me
+
+
+def feet_to_meters(feet: float) -> float:
+    """
+
+    :param feet:
+    :return:
+    """
+    pass  # implement me
+
+
+def kilometer_to_miles(kilometers: float) -> float:
+    """
+
+    :param kilometers:
+    :return:
+    """
+    pass  # implement me
+
+
+def miles_to_kilometers(miles: float) -> float:
+    """
+
+    :param miles:
+    :return:
+    """
+    # pass  # implement me
+    return round(miles / 0.62137, 2)
+
+
+def kilos_to_pounds(kilos: float) -> float:
+    """
+
+    :param kilos:
+    :return:
+    """
+    # pass  # implement me
+    return
+
+
+def pounds_to_kilos(pounds: float) -> float:
+    """
+
+    :param pounds:
+    :return:
+    """
+    pass  # implement me
