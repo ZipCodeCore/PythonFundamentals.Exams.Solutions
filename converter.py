6dacfd0e0154ce77a509954260f8ec3fd80b79b7
Roberto DeDeus <roberto.p.dedeus@gmail.com>
2020-03-09 14:18:04 -0400

    Add docstrings to converter.py functions.

--- converter.py
+++ converter.py
@@ -1,39 +1,48 @@
 def meters_to_feet(meters: float) -> float:
     """
+    This function takes a float representing a measurement in meters
+    and returns the corresponding value converted to feet.
+    The result is rounded to 2 decimal places.
 
-    :param meters:
-    :return:
+    :param meters: A float representing a measurement in meters.
+    :return: A float representing the input measurement converted to feet.
     """
-    # pass  # implement me
     return round(meters * 3.2808, 2)
 
 
 def feet_to_meters(feet: float) -> float:
     """
+    This function takes a float representing a measurement in feet
+    and returns the corresponding value converted to meters.
+    The result is rounded to 2 decimal places.
 
-    :param feet:
-    :return:
+    :param feet: A float representing a measurement in feet.
+    :return: A float representing the input measurement converted to meters.
     """
     return round(feet / 3.2808, 2)
 
 
 def kilometer_to_miles(kilometers: float) -> float:
     """
+    This function takes a float representing a measurement in kilometers
+    and returns the corresponding value converted to miles.
+    The result is rounded to 2 decimal places.
 
-    :param kilometers:
-    :return:
+    :param kilometers: A float representing a measurement in kilometers.
+    :return: A float representing the input measurement converted to miles.
     """
-    # pass  # implement me
     return round(kilometers * 0.62137, 2)
 
 
 def miles_to_kilometers(miles: float) -> float:
     """
+    This function takes a float representing a measurement in miles
+    and returns the corresponding value converted to kilometers.
+    The result is rounded to 2 decimal places.
 
-    :param miles:
-    :return:
+    :param miles: A float representing a measurement in miles.
+    :return: A float representing the input measurement converted to kilometers.
     """
-    # pass  # implement me
     return round(miles / 0.62137, 2)
 
 
