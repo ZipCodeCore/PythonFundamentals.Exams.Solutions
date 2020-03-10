09d1e510894eff754b4752750ac198b091a32b82
Roberto DeDeus <roberto.p.dedeus@gmail.com>
2020-03-10 10:09:03 -0400

    Add generate_list_with_strategy function to loopty_loop.

--- exam1/loopty_loop.py
+++ exam1/loopty_loop.py
@@ -1,14 +1,32 @@
+from typing import Callable, List
 
-def generate_list(start: int, stop: int, step: int = 1):
+
+def generate_list(start: int, stop: int, step: int = 1) -> List[int]:
     """
     Generate a list of integers.
 
-    :param start: Where to start the list (inclusive)
-    :param stop: Where to stop the list (exclusive)
+    :param start: Where to start the list (inclusive).
+    :param stop: Where to stop the list (exclusive).
     :param step: How many digits apart each number is from the others around it.
     :return: A list of integers.
     """
     result = []
     for i in range(start, stop, step):
         result.append(i)
+    return result
+
+
+def generate_list_with_strategy(start: int, stop: int, step: int, strategy: Callable) -> List[int]:
+    """
+    Generate a list of integers.
+
+    :param start: Where to start the list (inclusive).
+    :param stop: Where to stop the list (exclusive).
+    :param step: How many digits apart each number is from the others around it.
+    :param strategy: A function to manipulate each digit .
+    :return: A list of integers.
+    """
+    result = []
+    for i in range(start, stop, step):
+        result.append(int(strategy(i)))
     return result
